ad602a3ccbfcaa4afa0f5acbe503b2480f569f39
TanishqDhawan0912 <155245805+TanishqDhawan0912@users.noreply.github.com>
2024-06-09 08:45:44 +0000

    primitive datatypes done

---
+++ primitivedatatypes.js
@@ -0,0 +1,16 @@
+// 7 types = number , string , boolean , null , undefined , Symbol , BigInt 
+ 
+// const score = 100 => int 
+
+// cont score = 100.3 => float
+
+// const isLoggedIn = false => boolean
+
+// const value = null => null
+
+// let length => undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
