3765e1ab76df7934cffea4874ee0ea1750f849c1
TanishqDhawan0912 <155245805+TanishqDhawan0912@users.noreply.github.com>
2024-06-09 08:37:28 +0000

    datatypeconversion done

---
+++ datatypeconversion.js
@@ -0,0 +1,29 @@
+// let score = 33 => 33
+// let score = "33" => 33
+// let score = "33hello" => NaN
+// let score = "hello" => NaN
+// let score = "33.5" => 33.5
+// let score = null => 0
+// let score = undefined => NaN
+// let score = true => 1
+// let isNumber = Number(score)
+// console.log(isNumber);
+
+
+
+// let whether = 1 => true
+// let whether = 0 => false
+// let whether = -1 => true
+// let whether = "string" => true
+// let whether = "" => false
+// let whether = null or NaN or undefined => false
+// let isBool = Boolean(whether)
+// console.log(isBool);
+
+
+// let para = 33 => "33"
+// let para = "hello12" => "hello12"
+// let para = true => "true"
+// let para = null => "null"
+let isString =String(para)
+console.log(isString)
