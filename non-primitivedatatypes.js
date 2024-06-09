c3b73f8a6a9cdf0e18411a5290643daa15979b38
TanishqDhawan0912 <155245805+TanishqDhawan0912@users.noreply.github.com>
2024-06-09 09:16:30 +0000

    non-primitive data types done

---
+++ non-primitivedatatypes.js
@@ -0,0 +1,18 @@
+// Arrays , Objects , Functions
+
+// const avengers =["thor","hulk","ironman"]
+// console.log(avengers)
+
+// const avenger = {
+//     year:2012,
+//     type:"action",
+//     language:"english"
+// }
+
+// console.log(avenger)
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(myFunction())
