a92cfdcf601923d8ede3e26bd9e1908b4a743190
TanishqDhawan0912 <155245805+TanishqDhawan0912@users.noreply.github.com>
2024-06-11 13:23:44 +0000

    date and time done

---
+++ dateandtime.js
@@ -0,0 +1,16 @@
+let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.toJSON())
+console.log(mydate.getMonth()+1)
+console.log(mydate.getDay()+1)
+console.log(mydate.getDate()+1)
+
+// let mycreatedate = new Date (2023,0,23)
+// console.log(mycreatedate)
+
+// let mycreatedate = new Date (2023,0,23,5,3)
+// console.log(mycreatedate.toLocaleString())
