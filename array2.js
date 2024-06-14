aa7d7fa55d7c8f2d4cb01cd441e4d4d5400ca0f2
TanishqDhawan0912 <155245805+TanishqDhawan0912@users.noreply.github.com>
2024-06-14 11:30:57 +0000

    array 2 done

---
+++ array2.js
@@ -0,0 +1,24 @@
+const marvel_heros=["thor", "captain america", "spiderman"] 
+const dc_heros = ["superman" , "flash" , "batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+// console.log(marvel_heros.concat(dc_heros))
+
+
+
+// ++++++++ spread operator +++++++++
+// console.log([...marvel_heros,...dc_heros])
+
+
+
+// flat function in array
+// const arr = [1,2,[3,4],5,6,[7,8,[9,10]]];
+// console.log(arr.flat(Infinity))
+
+// console.log(Array.isArray(['hello','i','am']))
+
+// console.log(Array.from("hello i am good"))
+
+console.log(Array.of(100,101,102))
